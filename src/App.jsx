82fb06f34ff468a68948787d89b0378d2897ad41
abdelrhman-ehab <abdelrhmanehab2904@gmail.com>
2025-12-17 19:05:08 +0200

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Button } from '@heroui/react'
 import './App.css'
+import FeedPage from './Pages/FeedPage';
+import PostsContextProvider, { PostsContext } from './Context/context';
+
+
 
 function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+  return <>
+    <PostsContextProvider>
+      <FeedPage />
+    </PostsContextProvider>
+  </>
 }
 
 export default App
--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PostsContext } from '../Context/context'
 
 export default function FeedPage() {
+
+  let {allPosts}=useContext(PostsContext)
+  console.log(allPosts)
   return (
     <div>FeedPage</div>
   )
 }
+
